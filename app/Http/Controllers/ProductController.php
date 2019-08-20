f85d6d1c4f00e81f9d77ac4bc1a2d3b04a5304f2
Cem Gündüzoğlu <cmg.web@gmail.com>
2019-08-20 16:45:20 +0300

    View yapısı kodları eklendi

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,15 @@ class ProductController extends Controller
 {
     public function show($id, $r_type = 'test')
     {
-        return "Product Id: $id, Type: $r_type";
+        //return "Product Id: $id, Type: $r_type";
+        //return view('product', ['id'=> $id, 'name'=> 'Product 1', 'r_type'=> $r_type]);
+        
+        $name = 'Product 1';
+        
+        $categories = ['Category 1', 'Category 2', 'Category 3'];
+        
+        return view('product', compact('id', 'name', 'r_type', 'categories'));
+        
+        //return view('product')->with('id', $id)->with('name', $name)->with('r_type', $r_type);
     }
 }
