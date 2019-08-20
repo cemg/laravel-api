9e8663be07c94e48999f11b614e2fc40a74f52ce
Cem Gündüzoğlu <cmg.web@gmail.com>
2019-08-20 16:00:55 +0300

    Route yapısı örnekleri eklendi

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,34 @@
 
 Route::get('/', function () {
     return view('welcome');
+    //return redirect()->route('category.show', ['slug'=> 'books']);
+});
+
+Route::prefix('basics')->group(function() {
+
+    Route::get('/merhaba', function () {
+        return "Merhaba API";
+    });
+    
+    Route::get('/merhaba-json', function () {
+        return ['message' => "Merhaba API"];
+    });
+    
+    Route::get('/merhaba-json2', function () {
+        return response(['message' => "Merhaba API"], 200)
+            ->header('Content-Type', 'application/json');
+    });
+    
+    Route::get('/merhaba-json3', function () {
+        return response()->json(['message' => "Merhaba API"]);
+    });
+    
+    Route::get('/product/{id}/{type?}', function ($id, $r_type = 'test') {
+        return "Product Id: $id, Type: $r_type";
+    });
+    
+    Route::get('/category/{slug}', function($slug) {
+        return "Category Slug: $slug";
+    })->name('category.show');
+    
 });
