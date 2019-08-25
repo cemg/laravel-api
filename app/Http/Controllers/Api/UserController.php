bc6334e8f7e80f2a8b30b321e04232e7a0ffcba9
Cem Gündüzoğlu <cmg.web@gmail.com>
2019-08-25 16:53:32 +0300

    User API metotları düzenlendi

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,61 +5,96 @@ namespace App\Http\Controllers\Api;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $offset = $request->has('offset') ? $request->query('offset') : 0;
+        $limit = $request->has('limit') ? $request->query('limit') : 10;
+    
+        $qb = User::query();
+        if ($request->has('q'))
+            $qb->where('name', 'like', '%' . $request->query('q') . '%');
+    
+        if ($request->has('sortBy'))
+            $qb->orderBy($request->query('sortBy'), $request->query('sort', 'DESC'));
+    
+        $data = $qb->offset($offset)->limit($limit)->get();
+    
+        return response($data, 200);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
-        //
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+    
+        return response([
+            'data'    => $user,
+            'message' => 'User created.'
+        ], 201);
     }
-
+    
     /**
      * Display the specified resource.
      *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
+     * @param User $user
+     * @return User
      */
     public function show(User $user)
     {
-        //
+        return $user;
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param User $user
+     * @return Response
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+    
+        return response([
+            'data'    => $user,
+            'message' => 'User updated.'
+        ], 200);
     }
-
+    
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
+     * @param User $user
+     * @return Response
+     * @throws \Exception
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+    
+        return response([
+            'message' => 'User deleted'
+        ], 200);
     }
 }
