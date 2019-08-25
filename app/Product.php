7526ca35286d350125d5152305c6e9583769b4a8
Cem Gündüzoğlu <cmg.web@gmail.com>
2019-08-25 16:57:18 +0300

    categories ilişki fonksiyonu eklendi

--- app/Product.php
+++ app/Product.php
@@ -9,4 +9,8 @@ class Product extends Model
     //protected $table = 'products';
     //protected $fillable = ['name', 'slug', 'price'];
     protected $guarded = [];
+    
+    public function categories() {
+        return $this->belongsToMany('App\Category', 'product_categories');
+    }
 }
