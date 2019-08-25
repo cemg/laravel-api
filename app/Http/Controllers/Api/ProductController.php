156953548f8ec6315f4d9d779b819fce24013bdf
Cem Gündüzoğlu <cmg.web@gmail.com>
2019-08-25 16:51:31 +0300

    Product API metotları düzenlendi

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -2,40 +2,70 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Product;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //return Product::all();
         //return response()->json(Product::all(), 200);
-        return response(Product::all(), 200);
+        //return response(Product::all(), 200);
+        //return response(Product::paginate(10), 200);
+        
+        $offset = $request->has('offset') ? $request->query('offset') : 0;
+        $limit = $request->has('limit') ? $request->query('limit') : 10;
+        
+        $qb = Product::query();
+        if ($request->has('q'))
+            $qb->where('name', 'like', '%' . $request->query('q') . '%');
+    
+        if ($request->has('sortBy'))
+            $qb->orderBy($request->query('sortBy'), $request->query('sort', 'DESC'));
+        
+        $data = $qb->offset($offset)->limit($limit)->get();
+        
+        return response($data, 200);
     }
-
+    
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
-        //
+        //$input = $request->all();
+        //$product = Product::create($input);
+        
+        $product = new Product;
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->price = $request->price;
+        $product->save();
+        
+        return response([
+            'data'    => $product,
+            'message' => 'Product created.'
+        ], 201);
     }
-
+    
     /**
      * Display the specified resource.
      *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
+     * @param $id
+     * @return Response
      */
     public function show($id)
     {
@@ -44,29 +74,45 @@ class ProductController extends Controller
             return response($product, 200);
         else
             return response(['message' => 'Product not found!'], 404);
-            
+        
     }
-
+    
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Product $product
+     * @return Response
      */
     public function update(Request $request, Product $product)
     {
-        //
+        //$input = $request->all();
+        //$product->update($input);
+        
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->price = $request->price;
+        $product->save();
+        
+        return response([
+            'data'    => $product,
+            'message' => 'Product updated.'
+        ], 200);
     }
-
+    
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
+     * @param Product $product
+     * @return Response
+     * @throws \Exception
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        
+        return response([
+            'message' => 'Product deleted'
+        ], 200);
     }
 }
