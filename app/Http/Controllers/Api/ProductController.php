66ebb0e58ba7e5c978461142e8148531bcbb0b36
Cem Gündüzoğlu <cmg.web@gmail.com>
2020-07-27 23:43:26 +0300

    Return parametreleri güncellendi

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -25,52 +25,49 @@ class ProductController extends ApiController
         //return response()->json(Product::all(), 200);
         //return response(Product::all(), 200);
         //return response(Product::paginate(10), 200);
-        
+
         $offset = $request->has('offset') ? $request->query('offset') : 0;
         $limit = $request->has('limit') ? $request->query('limit') : 10;
-        
+
         $qb = Product::query()->with('categories');
         if ($request->has('q'))
             $qb->where('name', 'like', '%' . $request->query('q') . '%');
-        
+
         if ($request->has('sortBy'))
             $qb->orderBy($request->query('sortBy'), $request->query('sort', 'DESC'));
-        
+
         $data = $qb->offset($offset)->limit($limit)->get();
-        
+
         $data = $data->makeHidden('slug');
-        
+
         return response($data, 200);
     }
-    
+
     /**
      * Store a newly created resource in storage.
      *
      * @param Request $request
-     * @return Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
         //$input = $request->all();
         //$product = Product::create($input);
-        
+
         $product = new Product;
         $product->name = $request->name;
         $product->slug = Str::slug($request->name);
         $product->price = $request->price;
         $product->save();
-        
-        return response([
-            'data'    => $product,
-            'message' => 'Product created.'
-        ], 201);
+
+        return $this->apiResponse(ResultType::Success, $product, 'Product created.', 201);
     }
-    
+
     /**
      * Display the specified resource.
      *
      * @param $id
-     * @return Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
@@ -82,57 +79,52 @@ class ProductController extends ApiController
             return $this->apiResponse(ResultType::Error, null, 'Product Not Found!', 404);
         }
     }
-    
+
     /**
      * Update the specified resource in storage.
      *
      * @param Request $request
      * @param Product $product
-     * @return Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, Product $product)
     {
         //$input = $request->all();
         //$product->update($input);
-        
+
         $product->name = $request->name;
         $product->slug = Str::slug($request->name);
         $product->price = $request->price;
         $product->save();
-        
-        return response([
-            'data'    => $product,
-            'message' => 'Product updated.'
-        ], 200);
+
+        return $this->apiResponse(ResultType::Success, $product, 'Product updated.', 200);
     }
-    
+
     /**
      * Remove the specified resource from storage.
      *
      * @param Product $product
-     * @return Response
+     * @return \Illuminate\Http\JsonResponse
      * @throws \Exception
      */
     public function destroy(Product $product)
     {
         $product->delete();
-        
-        return response([
-            'message' => 'Product deleted'
-        ], 200);
+
+        return $this->apiResponse(ResultType::Success, null, 'Product deleted.', 200);
     }
-    
+
     public function custom1()
     {
         //return Product::select('id', 'name')->orderBy('created_at', 'desc')->take(10)->get();
         return Product::selectRaw('id as product_id, name as product_name')
             ->orderBy('created_at', 'desc')->take(10)->get();
     }
-    
+
     public function custom2()
     {
         $products = Product::orderBy('created_at', 'desc')->take(10)->get();
-        
+
         $mapped = $products->map(function ($product) {
             return [
                 '_id'           => $product['id'],
@@ -140,21 +132,21 @@ class ProductController extends ApiController
                 'product_price' => $product['price'] * 1.03
             ];
         });
-        
+
         return $mapped->all();
     }
-    
+
     public function custom3()
     {
         $products = Product::paginate(10);
-        
+
         return ProductResource::collection($products);
     }
-    
+
     public function listWithCategories()
     {
         $products = Product::with('categories')->paginate(10);
-        
+
         return ProductWithCategoriesResource::collection($products);
     }
 }
